14f1840dd22e91498a3eddbe9d9d4a9f15c39761
Alma <falcont40@hotmail.com>
2014-05-07 18:31:19 -0600

    More starmap methods added

--- JavaLEWrapper/src/javaLEWrapper/Wrapper/Map.java
+++ JavaLEWrapper/src/javaLEWrapper/Wrapper/Map.java
@@ -1,23 +1,61 @@
 package javaLEWrapper.Wrapper;
 
+import java.io.IOException;
+
 public class Map extends LESuperClass {
 	String url = "map";
-
+	String CheckStarForIncomingProbe(String sessionID, String starID){
+		StartOfObject(1, "check_star_for_incoming_probe");
+		String i = SessionAndBuildingIDRequests(sessionID, starID);
+		return i;
+	}
+	String GetStar(String sessionID, String starID){
+		StartOfObject(1, "get_star");
+		String i = SessionAndBuildingIDRequests(sessionID, starID);
+		return i;
+	}
+	String GetStarByName(String sessionID, String name){
+		StartOfObject(1, "get_star_by_name");
+		String i = SessionAndBuildingIDRequests(sessionID, name);
+		return i;
+	}
+	String SearchStars(String sessionID, String name){
+		StartOfObject(1, "search_stars");
+		String i = SessionAndBuildingIDRequests(sessionID, name);
+		return i;
+	}
+	String GetStars(String sessionID, String x1, String x2, String y1, String y2 ){
+		String b = "0";
+		try{
+			writer.beginObject();
+			writer.name("jsonrpc").value(2);
+			writer.name("id").value(1);
+			writer.name("method").value("get_stars");
+			writer.name("params").beginArray();
+			writer.value(sessionID);
+			writer.value(x1);
+			writer.value(x2);
+			writer.value(y1);
+			writer.value(y2);
+			writer.endArray();
+			writer.endObject();
+			writer.close();
+			b = gson.toJson(writer);
+			//writer.flush();
+			b = CleanJsonObject(b);
+		}catch(IOException e){
+			System.out.println("ioexception");
+		}catch(NullPointerException e){
+			System.out.println("null pointer exception");
+		}finally{
+		}
+		return b;
+	}
 }
 /*
 get_star_map(hash)
 get_stars ( session_id, x1, y1, x2, y2 )
-
-check_star_for_incoming_probe ( session_id, star_id )
-
-get_star (session_id, star_id)
-
-get_star_by_name (session_id, name)
-
 get_star_by_xy (session_id, x, y)
-
-search_stars (session_id, name)
-
 probe_summary_fissures(session_id, zone(optional))
 
 
