85c6bbfa3ffcc82de320e7f179659602d5dfb32d
Alma <falcont40@hotmail.com>
2014-05-09 17:40:10 -0600

    Security added
    Starting to add the Security Ministry Methods

---
+++ JavaLEWrapper/src/javaLEWrapper/Wrapper/Security.java
@@ -0,0 +1,30 @@
+package javaLEWrapper.Wrapper;
+
+public class Security extends Buildings {
+
+	Security(String buildingName) {
+		super("security");
+		// TODO Auto-generated constructor stub
+	}
+
+}
+/*
+Security Methods
+view_prisoners ( session_id, building_id, [ page_number ])
+session_id
+building_id
+page_number
+execute_prisoner ( session_id, building_id, prisoner_id )
+session_id
+building_id
+prisoner_id
+release_prisoner ( session_id, building_id, prisoner_id )
+session_id
+building_id
+prisoner_id
+view_foreign_spies ( session_id, building_id, [ page_number ])
+session_id
+building_id
+page_number
+
+*/
